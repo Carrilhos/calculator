6253d467dad29378ba860c7a2d3e18390d49b702
Gabriel Carrilhos <gcarrilhos@gmail.com>
2022-11-11 11:21:39 -0300

    v2

--- src/components/Screen/index.js
+++ src/components/Screen/index.js
@@ -1,12 +1,23 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { CalculatorContext } from "../../context/CalculatorContext";
-import { ScreenContainer } from "./styles";
+import { ScreenContainer, Aux } from "./styles";
 
 const Screen = () => {
   const { calc } = useContext(CalculatorContext);
 
+  useEffect(() => {
+    console.log("num", calc.num);
+    console.log("res", calc.res);
+    console.log("sign", calc.sign);
+  }, [calc]);
+
   return (
     <ScreenContainer>
+      <Aux>
+        {calc.num !== 0 && !calc.res && <span> {calc.num} </span>}
+        {calc.res !== 0 && <span> {calc.res} </span>}
+        {calc.sign && <span> {calc.sign}</span>}
+      </Aux>
       <h1>{calc.num ? calc.num : calc.res}</h1>
     </ScreenContainer>
   );
--- src/components/Screen/styles.js
+++ src/components/Screen/styles.js
@@ -5,4 +5,10 @@ export const ScreenContainer = styled.div`
   background-color: #c2c2f0;
   margin-bottom: 10px;
   padding: 0px 10px;
+  position: relative;
+`;
+
+export const Aux = styled.div`
+  position: absolute;
+  top: 2px;
 `;
